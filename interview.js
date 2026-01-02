ffcd089eaed199bdd33df198ea518b84e8088983
anil sidhu <anilsidhu@anils-MacBook-Air.local>
2026-01-02 22:43:09 +0530

    done

--- interview.js
+++ interview.js
@@ -1,5 +1,6 @@
-const x = [1, 2, 3]; 
-const y = Object.assign([],x) 
-y.push(4);
+const x = [[1, 2, 3],[4,5,6]]; 
+// const y = [[...x[0]],[...x[1]]]
+const y = x.map((item)=>[...item])
+y[0].push(4);
 
 console.log(x);
